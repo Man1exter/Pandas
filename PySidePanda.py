27a0f453231fc5436b214379e37a21632407c4bc
Mariusz Perzynski <m.perzyk@interia.pl>
2023-01-21 11:14:38 +0100

    set animation/ + setstyle

--- PySidePanda.py
+++ PySidePanda.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from PySide6.QtWidgets import QApplication, QFileDialog, QMainWindow, QPushButton
+from PySide6.QtCore import QRect, QPropertyAnimation
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -7,6 +8,14 @@ class MainWindow(QMainWindow):
         self.select_file_button = QPushButton("Select CSV file", self)
         self.select_file_button.clicked.connect(self.select_file)
         self.setCentralWidget(self.select_file_button)
+        
+        self.select_file_button.setStyleSheet("background-color: yellow; font-size: 25px;")
+        
+        animation = QPropertyAnimation(self.select_file_button, b"geometry")
+        animation.setDuration(10000)
+        animation.setStartValue(QRect(0, 0, 100, 30))
+        animation.setEndValue(QRect(250, 250, 100, 30))
+        animation.start()
 
     def select_file(self):
         options = QFileDialog.Options()
@@ -19,5 +28,7 @@ class MainWindow(QMainWindow):
 if __name__ == '__main__':
     app = QApplication()
     window = MainWindow()
+    window.setStyleSheet("background-color: lightblue;")
+    window.setFixedSize(500, 500)
     window.show()
-    app.exec_()
+    app.exec()
